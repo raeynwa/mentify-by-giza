ce2a9ba3fa84956c6df3dc66181d45879b38c1d7
raeynwa <raeynwa@gmail.com>
2023-03-09 22:29:54 +0700

    footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,62 @@
+<footer>
+    <div class="container py-5 bg-gray-300">
+        <div class="row">
+            <div class="col-md-3">
+                <img src="{{ asset('assets/images/logo/Logo.png') }}" alt="" class="mb-5">
+                <p>Copyright © 2021 Mentify by Giza Lab</p>
+            </div>
+
+            <div class="col-md-3">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Sitemap</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Bootcamp</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Mentify Lite</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Karya Alumni</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Kelas Saya</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col-md-3">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Our Team</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">FAQ</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col-md-3">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Get in Touch</a>
+                    </li>
+                    <li class="nav-item d-flex justify-content-start">
+                        <a class="nav-link" href="#"><i class="fa-brands fa-instagram fa-2x me-1"></i></a>
+                        <a class="nav-link" href="#"><i class="fa-brands fa-linkedin fa-2x"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
